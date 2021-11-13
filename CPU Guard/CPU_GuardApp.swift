174be148c31679fb91ca62dd6e9639c10ea7c27e
Cenk Alti <cenkalti@gmail.com>
2021-11-13 18:32:05 -0500

    Initial Commit

---
+++ CPU Guard/CPU_GuardApp.swift
@@ -0,0 +1,17 @@
+//
+//  CPU_GuardApp.swift
+//  CPU Guard
+//
+//  Created by Cenk Altı on 2021-11-13.
+//
+
+import SwiftUI
+
+@main
+struct CPU_GuardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
